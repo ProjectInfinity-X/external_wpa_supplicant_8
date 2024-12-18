Imaginovskiy/android_external_wpa_supplicant_8

wpa_supplicant: add support for bcmdhd SAE authentication offload
* Required to get WPA3 to work at all on bcmdhd devices

Change-Id: Ie96550256afd1596f26a377b4af104a8bd104c10

wpa_supplicant/Android.mk
@@ -16,6 +16,10 @@ ifeq ($(BOARD_WLAN_DEVICE), qcwcn)
   CONFIG_DRIVER_NL80211_QCA=y
 endif
 
+ifneq ($(BOARD_WLAN_BCMDHD_SAE),)
+  CONFIG_BCMDHD_SAE=y
+endif
+
 include $(LOCAL_PATH)/android.config
 
 # To ignore possible wrong network configurations
@@ -66,6 +70,11 @@ endif
 # Disable roaming in wpa_supplicant
 ifdef CONFIG_NO_ROAMING
 L_CFLAGS += -DCONFIG_NO_ROAMING
+endif
+
+# BCMDHD SAE authentication offload
+ifdef CONFIG_BCMDHD_SAE
+L_CFLAGS += -DCONFIG_BCMDHD_SAE
 endif
 
 # Use Android specific directory for control interface sockets

ProjectInfinity-X/external_wpa_supplicant_8

wpa_supplicant/Android.mk
@@ -18,6 +18,10 @@ ifeq ($(BOARD_WLAN_DEVICE), qcwcn)
   CONFIG_DRIVER_NL80211_QCA=y
 endif
 
+ifneq ($(BOARD_WLAN_BCMDHD_SAE),)
+  CONFIG_BRCM_SAE=y
+endif
+
 include $(LOCAL_PATH)/android.config
 
 # To ignore possible wrong network configurations
@@ -79,6 +83,11 @@ endif
 # source way after the multiple AKMs is fully supported by driver and verified.
 ifeq ($(WIFI_BRCM_OPEN_SOURCE_MULTI_AKM), enabled)
 L_CFLAGS += -DWIFI_BRCM_OPEN_SOURCE_MULTI_AKM
+endif
+
+# BCMDHD SAE authentication offload
+ifdef CONFIG_BRCM_SAE
+L_CFLAGS += -DCONFIG_BRCM_SAE
 endif
 
 # Use Android specific directory for control interface sockets